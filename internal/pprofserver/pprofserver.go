3524d30550aaeaffde6a056b5dfa7636f24cb9ea
Artem <neoritovka110@gmail.com>
2026-08-30 18:28:56 +0300

    feat: add new pprof metrics

--- internal/pprofserver/pprofserver.go
+++ internal/pprofserver/pprofserver.go
@@ -4,10 +4,13 @@ import (
 	"log"
 	"net/http"
 	"net/http/pprof"
+	"runtime"
 )
 
 func StartPprofServer(addr string) error {
 	mux := http.NewServeMux()
+	runtime.SetBlockProfileRate(1)
+	runtime.SetMutexProfileFraction(1)
 
 	mux.HandleFunc("/debug/pprof/", pprof.Index)
 	mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
@@ -15,6 +18,9 @@ func StartPprofServer(addr string) error {
 	mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
 	mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
 	mux.Handle("/debug/pprof/heap", pprof.Handler("heap"))
+	mux.Handle("/debug/pprof/goroutine", pprof.Handler("goroutine"))
+	mux.Handle("/debug/pprof/mutex", pprof.Handler("mutex"))
+	mux.Handle("/debug/pprof/block", pprof.Handler("block"))
 
 	server := &http.Server{
 		Addr:    addr,
